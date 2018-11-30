620ccbe9d91503657c23899199663331a6779177
salman zafar <salmanzafar949@gmail.com>
2018-11-30 14:54:28 +0500

    User Registration Completed

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            # password = form.cleaned_data.get('password')
+            messages.success(request, f'Account created for {username}..!')
+            return redirect('blog:index')
+    else:
+        form = UserForm()
+    return render(request, 'users/register.html', {'form': form})
