37cbfae0ddd5d8b7c9e72cae317d54ce57e60183
salman zafar <salmanzafar949@gmail.com>
2018-11-30 17:02:00 +0500

    signal to perform some specific jobs

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+    profile_pic = models.ImageField(default='profile_pics/default.jpeg', upload_to='profile_pics')
 
     def __str__(self):
         return self.user.username
