00cca94f3998afd70837a956d9266b2d0acc49b9
salman zafar <salmanzafar949@gmail.com>
2018-12-03 16:53:22 +0500

    List and Detail view for posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name="blog"
 
 urlpatterns = [
 
-    path('', views.index, name="index"),
+    path('', views.PostListView.as_view(), name="index"),
+    path('post/<int:pk>/detail/', views.PostDetailView.as_view(), name="detail"),
     path('about', views.about, name="about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts': posts})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ['-created_at']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
 
 
 def about(request):
