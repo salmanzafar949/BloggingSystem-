2905398a32ea98e97cc837aad2d53863a4bb8902
salman zafar <salmanzafar949@gmail.com>
2018-12-04 18:40:05 +0500

    Reset Password implemented

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -131,3 +131,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog:index'
 
 LOGIN_URL ='users:login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = ""
+
+EMAIL_HOST_PASSWORD = ""
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 app_name="users"
 
@@ -9,5 +9,9 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('login', LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout', LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path('password/reset', PasswordResetView.as_view(template_name="users/password_reset.html"), name="reset-pass"),
+    path('password/reset/done', PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="reset-pass-done"),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="reset-pass-confirm"),
+    path('password/reset/updated/', PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="reset-comp"),
     path('profile', views.userprofile, name="profile")
 ]
